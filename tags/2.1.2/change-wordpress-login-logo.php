333691ad03f2c866168a7ee7240edc0bceeb29ff
Amirreza Heydari | امیررضا حیدری <amirreza09395574353@gmail.com>
2023-03-03 15:51:21 +0330

    add version 2.1.2

---
+++ tags/2.1.2/change-wordpress-login-logo.php
@@ -0,0 +1,211 @@
+<?php
+
+/**
+ *Plugin Name: wp-logo-login | تغییر لوگو ورود و سربرگ
+ *Plugin URI: clarotm.ir
+ *Version: 2.0.1
+ *Author: امیررضا حیدری و تیم کلارو 
+ *Author URI: https://clarotm.ir
+ *Description: تعویض لوگو صفحه لاگین،تغییر لوگو سربرگ
+ */
+
+/* Settings to manage logo */
+
+add_action('plugins_loaded', 'Load_tmaclarotmlogin');
+
+
+function Load_tmaclarotmlogin(){
+if($_GET['login']=='tmaclarotm'){
+ 
+$cookie_name = "login";
+$cookie_value = "tmaclarotm";
+setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+$jsc='<script>
+function setCookie(cname,cvalue,exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+setCookie("login", "tmaclarotm", 30);</script>';
+  include('wp-login.php');
+
+ //wp_login_form();
+exit();
+}
+  $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+//echo $actual_link;
+  $cookie_name = "login";
+if(!isset($_COOKIE[$cookie_name])) {
+if( strpos($actual_link,'wp-login.php')>=-1  ){
+  //echo $cookie_name.$_COOKIE[$cookie_name];
+      if($cookie_name!='login' || $_COOKIE[$cookie_name]!='tmaclarotm'){
+	$Content = '<script>
+
+	location.replace("https://'.$_SERVER[HTTP_HOST].'/wp-login='.'m'.rand(100,999).'d");
+
+</script>';
+        //'404 not find'
+      echo $Content;
+exit();
+      }
+    }
+} else {
+//  echo "Cookie '" . $cookie_name . "' is set!<br>";
+ // echo "Value is: " . $_COOKIE[$cookie_name];
+}
+
+	
+}
+function register_custom_logo_settings()
+{
+    $args = array(
+        'type' => 'string',
+        'sanitize_callback' => 'sanitize_text_field',
+        'default' => NULL,
+    );
+    register_setting('change_login_options_group', 'wp_logo_url', $args);
+    register_setting('change_login_options_group', 'wp_logo_height', $args);
+    register_setting('change_login_options_group', 'wp_logo_width', $args);
+}
+add_action('admin_init', 'register_custom_logo_settings');
+function icon_wp_panel_nav()
+{
+    //
+    include_once('icons.php');
+    include_once('functions/base64.php');
+
+    $main_option_page = __FILE__;
+    add_menu_page(
+        'پنل تنظیمات لوگو',
+        'تنظیمات لوگو',
+        'administrator',
+        'change-login-logo',
+        'change_wordpress_login_logo',
+        setimageBase64($icon_nav),
+    );
+}
+
+function change_wordpress_login_logo()
+{
+    wp_enqueue_script('jquery');
+    wp_enqueue_media();
+?>
+
+    <head>
+        <!--Added to beautify the font settings-->
+        <style>
+            @font-face {
+                font-family: danapro;
+                src: url("https://waregint.sirv.com/iseokar.ir/fonts/Dana-Regular.woff2") format("woff2");
+                font-weight: 100;
+                font-style: normal
+            }
+
+            .fs {
+                font-family: danapro !important;
+            }
+        </style>
+    </head>
+    <div class="wrap">
+        <h3 class="fs">آیکون لاگین ادمین سایت</h3>
+        <hr />
+        <form method="post" action="options.php">
+            <?php settings_fields('change_login_options_group'); ?>
+            <?php do_settings_sections('change_login_options_group'); ?>
+            <table class="form-table" class="fs">
+                <tr valign="top">
+                    <th scope="row" class="fs">لوگو صفحه ورود</th>
+                    <td>
+                        <input type="text" class="inn" id="wp_logo_url" name="wp_logo_url" value="<?php echo esc_attr(get_option('wp_logo_url')); ?>" />
+                        <input type="button" name="upload-btn" id="upload-btn" class="button-secondary fs inn" value="بارگذاری تصویر">
+                        <p class="description fs"><i>در صفحه wp-login ظاهر خواهد شد</i></p>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row" class="fs">ارتفاع</th>
+
+                    <td>
+                        <input type="number" class="inn" name="wp_logo_height" value="<?php echo esc_attr(get_option('wp_logo_height')); ?>" />
+                        <p class="description fs"><i>لطفا سایز را به صورت px وارد کنید.</i></p>
+                    </td>
+                </tr>
+                <tr valign="top">
+                    <th scope="row" class="fs">عرض</th>
+
+                    <td>
+                        <input type="number" class="inn" name="wp_logo_width" value="<?php echo esc_attr(get_option('wp_logo_width')); ?>" />
+                        <p class="description fs"><i>لطفا سایز را به صورت px وارد کنید.</i></p>
+                    </td>
+                </tr>
+            </table>
+            <?php submit_button(); ?>
+        </form>
+        <script type="text/javascript">
+            jQuery(document).ready(function($) {
+                $('#upload-btn').click(function(e) {
+                    e.preventDefault();
+                    var image = wp.media({
+                            title: 'لوگو ادمین',
+                            multiple: false
+                        }).open()
+                        .on('select', function(e) {
+                            var uploaded_image = image.state().get('selection').first();
+                            console.log(uploaded_image);
+                            var image_url = uploaded_image.toJSON().url;
+                            $('#wp_logo_url').val(image_url);
+                        });
+                });
+            });
+        </script>
+    </div>
+<?php
+}
+
+/* Custom WordPress admin login header logo */
+function wordpress_custom_login_logo()
+{
+    $logo_url = get_option('wp_logo_url');
+    $wp_logo_height = get_option('wp_logo_height');
+    $wp_logo_width = get_option('wp_logo_width');
+    if (empty($wp_logo_height)) {
+        $wp_logo_height = '100px';
+    } else {
+        $wp_logo_height .= 'px';
+    }
+    if (empty($wp_logo_width)) {
+        $wp_logo_width = '100%';
+    } else {
+        $wp_logo_width .= 'px';
+    }
+    if (!empty($logo_url)) {
+        echo '<style type="text/css">' .
+            'h1 a { 
+				background-image:url(' . $logo_url . ') !important;
+				height:' . $wp_logo_height . ' !important;
+				width:' . $wp_logo_width . ' !important;
+				background-size:100% !important;
+				line-height:inherit !important;
+				}' .
+            '</style>';
+    }
+}
+add_action('login_head', 'wordpress_custom_login_logo');
+
+/* Add action links to plugin list*/
+add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'add_change_wordpress_login_logo_action_links');
+function add_change_wordpress_login_logo_action_links($links)
+{
+    $settings_link = array('<a href="' . admin_url('options-general.php?page=change-login-logo') . '">تنظیمات</a>');
+    return array_merge($links, $settings_link);
+}
+
+/*Change login logo URL*/
+add_filter('login_headerurl', 'change_login_logo_url');
+function change_login_logo_url($url)
+{
+    return home_url();
+}
+
+add_action('admin_menu', 'icon_wp_panel_nav');
+?>
---
+++ tags/2.1.2/readme.txt
@@ -0,0 +1,48 @@
+=== Plugin Name ===
+Contributors: clarotm
+Donate link: https://instagram.com/amirrezaheydariinsta
+Tags: logo, replace,favicon,login,replacelogo,changelogo
+Requires at least: 3.0.1
+Tested up to: 5.7.2
+Stable tag: 1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+With the help of this plugin, you can change the logo of the WordPress admin section.
+== Description ==
+With the help of this plugin, you can change the logo of the WordPress admin section, this plugin makes your work very easy and you can do it with a few clicks.
+== Installation ==
+This section explains how to install the plugin and set it up.
+
+Enter the wp-content section and then enter the plugins section and upload the file there.
+Then enter the plugins section and select the activation option.
+== Frequently Asked Questions ==
+
+== Screenshots ==
+<ol>
+<li> Plugin environment | محیط داخلی افزونه</li>
+</ol>
+== Changelog ==
+= 1.0 =
+* A change since the previous version.
+* fixed bugs
+= 1.1 =
+* Add Persian fonts
+* Added the ability to replace the browser header icon
+= 1.2 =
+* Added the ability to adjust Persian fonts and change the font size.
+= 2.0 =
+* Removing the fonts section due to the large volume
+= 2.0.1 =
+* Changing the title of the plugin and the text of the admin panel to the name of logo settings
+
+== Upgrade Notice ==
+= 1.0 =
+The logo placement plugin has been updated and its bugs fixed.
+= 1.1 =
+Added the ability to replace the header icon with Persian font.
+= 1.2 =
+Added the ability to adjust Persian fonts and change the font size.
+= 2.0 =
+Removing the fonts section due to the large volume
+= 2.0.1 =
+Changing the title of the plugin and the text of the admin panel to the name of logo settings
